2bc489db90f5725d77099a85eea65aaebab1d961
Ponfa Michael Joseph <ponfa_m@outlook.com>
2024-09-17 17:50:02 +0100

    ludoGame Contract Done

--- contracts/ludoGame.sol
+++ contracts/ludoGame.sol
@@ -1,7 +1,54 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.20;
 
-contract ludoGame {
+contract LudoGame {
+    address[4] public players; 
+    uint256 public firstPlayer; 
+    uint8 public rollDice; 
+    bool public start;
+
+    mapping(address => bool) public isPlayer; 
+    mapping(address => uint256) public playerPosition; 
+
+    modifier players() {
+        require(isPlayer[msg.sender], "You are not a player in this game.");
+        _;
+    }
+
+    modifier turn() {
+        require(players[firstPlayer] == msg.sender, "It's not your turn.");
+        _;
+    }
+
+    modifier gameInPlay() {
+        require(start, "The game has not started yet.");
+        _;
+    }
+
+    constructor(address[4] memory _players) {
+        players = _players;
+        for (uint256 i = 0; i < 4; i++) {
+            isPlayer[players[i]] = true;
+        }
+        firstPlayer = 0; 
+        start = true;
+    }
+
+    function rollDice() public players turn gameInPlay  returns (uint256) {
+        rollDice = uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.number, msg.sender))) % 6) + 1;
+        playerPosition[msg.sender] += rollDice; 
+        
+        firstPlayer = (firstPlayer + 1) % 4;
+
+        return rollDice;
+    }
+
+    function getCurrentPlayer() public view returns (address) {
+        return players[firstPlayer];
+    }
+
+    function getPlayerPosition(address player) public view returns (uint256) {
+        return playerPosition[player];
+    }
 
 }
